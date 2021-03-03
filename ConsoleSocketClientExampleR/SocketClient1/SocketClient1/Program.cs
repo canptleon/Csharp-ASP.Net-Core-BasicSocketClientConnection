e844c69f84c9239420f88586b9e154d3d7831615
canptleon <ardakeyisoglu@gmail.com>
2021-03-03 17:17:42 +0300

    +0

---
+++ ConsoleSocketClientExampleR/SocketClient1/SocketClient1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        const int PORT_NO = 3000;
+        const string SERVER_IP = "192.168.1.65";
+        static void Main(string[] args)
+        {
+            string textToSend = DateTime.Now.ToString();
+
+            TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+            NetworkStream nwStream = client.GetStream();
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+            Console.WriteLine("Sending : " + textToSend);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
---
+++ ConsoleSocketClientExampleR/SocketExample1/SocketExample1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        const int PORT_NO = 3000;
+        const string SERVER_IP = "192.168.1.65";
+
+        static void Main(string[] args)
+        {
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            TcpListener listener = new TcpListener(localAdd, PORT_NO);
+            Console.WriteLine("Server Started...");
+            listener.Start();
+
+            TcpClient client = listener.AcceptTcpClient();
+
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+            string datareceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("received : " + datareceived);
+
+            Console.WriteLine("Hoşgeldiniz.");
+            nwStream.Write(buffer, 0, bytesRead);
+
+            client.Close();
+            listener.Stop();
+
+            Console.ReadLine();
+        }
+    }
+}
